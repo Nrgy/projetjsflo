3ae1e8bde594ae749a89d8c6f1ce81e5d4a5cf43
Nrgy <cedric.burceaux@goood.pro>
2017-04-23 13:51:57 +0200

    Modal à l'extérieur du bloc photo Début de la récupération de la photo dans la modal

--- LIFAP5-Projet.js
+++ LIFAP5-Projet.js
@@ -90,8 +90,22 @@ $("#panel-gallery").on("click", ".col-sm-2", function(element){// Utilisation de
     var number = photoName[1];
     $('#panel-photo').empty();
     var name = $("#"+ element.target.id).data("name");
-    $('#panel-photo').append('<img src="'+ element.target.src +'" class="img-responsive" alt="Photo '+ number +'"><h4>'+ name +'</h4><span class="text-muted">Photo '+ number +'</span><br><span class="text-muted">'+ element.target.title +'</span><br>');
+    var identifier = $("#"+ element.target.id).data("identifier");
+    $('#panel-photo').append('<img src="'+ element.target.src +'" class="img-responsive" alt="Photo '+ number +'"><h4>'+ name +'</h4><span class="text-muted">Photo '+ number +'</span><br><span class="text-muted">'+ element.target.title +'</span><br><button type="button" id="edit-modal" data-id='+ identifier +' data-name='+ name +' data-photoname="Photo '+ number +'" data-desc='+ photoName+' data-albums=' + element.target.title + ' class="btn btn-info btn-lg">Edit</button>');
   });
+
+$(".container-fluid").on("click","#edit-modal", function(element){
+  $('#photoModal').modal();
+  var id = $("#"+ element.target.id).data("id");
+  var name = $("#"+ element.target.id).data("name");
+  var desc = $("#"+ element.target.id).data("desc");
+  var photoname = $("#"+ element.target.id).data("photoname");
+  var albums = $("#"+ element.target.id).data("albums");
+  var selectedPhoto =  {"_id":{"$id": "" + id + ""},"name":"" + name + "","desc":"" + photoname + "","albums":albums}
+  $('.modal-title').html(selectedPhoto.name);
+  $('#new-desc').val(selectedPhoto.desc);
+  $('#new-albums').val(selectedPhoto.albums);
+});
 
   $("#upload-button").click(function() {
     let formElt = document.getElementById("upload-form"); //$("#upload-form")[0];
@@ -127,7 +141,7 @@ $("#panel-gallery").on("click", ".col-sm-2", function(element){// Utilisation de
         if(tags.indexOf(album) === -1)
           tags.push(album);
 			});
-  		  $('#panel-gallery .row').append('<div class="col-sm-2"><img src="http://134.214.200.137/images/' + image._id.$id + '/' + image.name +'" class="img-thumbnail" data-name="'+image.name+'" data-toggle="tooltip" title="' + albums + '" id="photo-' + i +'" alt="' + image.desc + '"><h4>' + image.name +'</h4><span class="text-muted">' + image.desc + '</span></div>');
+  		  $('#panel-gallery .row').append('<div class="col-sm-2"><img src="http://134.214.200.137/images/' + image._id.$id + '/' + image.name +'" class="img-thumbnail" data-name="'+image.name+'" data-identifier="' +  image._id.$id + '" data-toggle="tooltip" title="' + albums + '" id="photo-' + i +'" alt="' + image.desc + '"><h4>' + image.name +'</h4><span class="text-muted">' + image.desc + '</span></div>');
 		i++;
 });
     tags.forEach(function(tag){
